0d4adce9c237830bde48b2401c79fe3a05cc28f8
auronvila <auronvila1121@gmail.com>
2024-06-16 09:35:38 +0300

    implemented bubble sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,14 +1,11 @@
 export default function bubble_sort(arr: number[]): void {
-  let swapped: boolean;
-  do {
-    swapped = false;
-    for (let i = 0; i < arr.length - 1; i++) {
-      if (arr[i] > arr[i + 1]) {
-        let temp = arr[i];
-        arr[i] = arr[i + 1];
-        arr[i + 1] = temp;
-        swapped = true;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
       }
     }
-  } while (swapped)
+  }
 }
