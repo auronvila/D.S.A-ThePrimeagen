066305dba0f8556035a600a58379dc2c4979fe98
auronvila <auronvila1121@gmail.com>
2024-06-19 09:48:38 +0300

    implemented Stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,44 @@
+type QNode<T> = {
+  value: T;
+  prev?: QNode<T>
+}
 export default class Stack<T> {
-    public length: number;
+  public length: number;
+  public head?: QNode<T>
 
-    
 
-    constructor() {
+  constructor() {
+    this.length = 0;
+    this.head = undefined;
+  }
+
+  push(item: T): void {
+    this.length++;
+    const node = {value: item, next: this.head} as QNode<T>;
+    if (!this.head) {
+      this.head = node;
+      return
     }
 
-    push(item: T): void {
+    node.prev = this.head;
+    this.head = node;
+  }
 
-}
-    pop(): T | undefined {
+  pop(): T | undefined {
+    this.length = Math.max(0,this.length - 1)
+    if (this.length === 0) {
+      const head = this.head;
+      this.head = undefined;
+      return head?.value
+    }
 
-}
-    peek(): T | undefined {
+    const head = this.head;
+    this.head = this.head?.prev;
 
-}
+    return head?.value;
+  }
+
+  peek(): T | undefined {
+    return this.head?.value;
+  }
 }
